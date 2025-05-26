812c67776f015f6ba4be93aef43b59d3b33d5040
Meysam <meysampishnamaz@gmail.com>
2025-05-26 17:26:14 +0330

    Created about you screen to get user data.

---
+++ app/src/main/java/com/dante/Macrovia/AboutYouScreen.kt
@@ -0,0 +1,58 @@
+package com.dante.Macrovia
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AboutYouScreen(modifier: Modifier = Modifier, onBackClicked: () -> Unit) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = onBackClicked) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Text(
+                "About You",
+                style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold),
+                modifier = Modifier.align(Alignment.CenterVertically)
+            )
+
+            Spacer(modifier = Modifier.weight(1.5f))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun preeeev() {
+        AboutYouScreen(onBackClicked = {})
+    }
---
+++ app/src/main/java/com/dante/Macrovia/utilities/Screen.kt
@@ -0,0 +1,7 @@
+package com.dante.Macrovia.utilities
+
+object Screen {
+    const val OnBoardingScreen = "on_boarding_screen"
+    const val AboutYou = "about_you_screen"
+
+}
