e280b6d1f4ef59907bbcd992fc025b0ec01c89d1
Meysam <meysampishnamaz@gmail.com>
2025-05-26 17:25:10 +0330

    Created first onboarding screen.

---
+++ app/src/main/java/com/dante/Macrovia/OnboardingScreen.kt
@@ -0,0 +1,65 @@
+package com.dante.Macrovia
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier, onGetStartedClicked: () -> Unit) {
+    Column(
+        modifier = modifier
+            .padding(32.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Macrovia", style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold))
+            Spacer(Modifier.height(32.dp))
+                Text(
+                    "Track your nutrition and achieve your health goals.",
+                    style = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center
+                )
+        }
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(R.drawable.macrovia_logo),
+                contentDescription = "Macrovia Logo",
+                modifier = Modifier
+                    .size(150.dp)
+            )
+            Spacer(Modifier.height(12.dp))
+            Text("Macrovia", style = MaterialTheme.typography.displayLarge)
+        }
+        Button(
+            onClick = onGetStartedClicked,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        ) {
+            Text("Get Started", style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold))
+        }
+    }
+}
